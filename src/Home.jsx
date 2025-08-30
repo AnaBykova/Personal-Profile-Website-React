f463032efb5dba81c1b581adec396a273f561d3a
Ana Bykova <nastiashko@gmail.com>
2025-08-30 16:51:34 -0600

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+
+function Home() {
+  return (
+    <section id = "home">
+      <div>
+          <h5 className="card-title">I'm</h5>
+          <div id = "home-text">
+            Full Stack <br />
+            Software<br />
+            Developer<br />
+          </div>
+      </div>
+    </section>
+    
+  )
+}
+
+export default Home
